aa3389c940ee0bce3dcb6b8b11d93eba904e3032
Pratik Raj <pratikraj220011@gmail.com>
2025-04-12 19:59:13 +0530

    redis updated

--- Server/dist/config/redis.config.js
+++ Server/dist/config/redis.config.js
@@ -1,6 +1,4 @@
 import { Redis } from 'ioredis';
-const redis = new Redis({
-    host: 'localhost',
-    port: 6379
-});
+const redis = new Redis(process.env.REDIS_URL);
+console.log("redis: ", redis);
 export default redis;
